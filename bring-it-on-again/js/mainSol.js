d752ed2df014405429790761536645138409c9ea
coding4freedom <chawkins2010@hotmail.com>
2022-04-07 23:54:52 -0500

    bring it on again try 1

--- bring-it-on-again/js/mainSol.js
+++ bring-it-on-again/js/mainSol.js
@@ -1,23 +1,13 @@
 // *Variables*
 // Declare a variable, assign it a value, and alert the value
-let cap = '5'
-alert(cap)
+
 // Create a variable, divide it by 10, and console log the value
-let product = 50;
-console.log(product / 10)
+
 // *Functions*
 // Create a function that multiplys 3 numbers and alerts the product
-function multiplyNum(x, y, z){
-    alert(x * y * z)
-}
-multiplyNum(2, 3, 5)
+
 // Create a function that takes in 4 numbers. Add the first two numbers and subtract the next two. Console log the result
-function multi(a, b, c, d){
-    let result1 = (a + b)
-    let result2 = c - d
-    console.log(result1, result2)
-}
-multi(5, 5, 10, 3)
+
 // *Conditionals*
 // Create a function that takes in 3 numbers. Starting with 100 add the first number, subtract the second, and divide the third. If the value is greater then 25, console log "WE HAVE A WINNNA"
 
