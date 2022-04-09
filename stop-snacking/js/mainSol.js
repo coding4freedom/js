5298e1ccbeb4ffa3e79a11be8f81aba78466065d
coding4freedom <chawkins2010@hotmail.com>
2022-04-08 21:31:02 -0500

    stop snacking try 1

--- stop-snacking/js/mainSol.js
+++ stop-snacking/js/mainSol.js
@@ -1,14 +1,14 @@
 //Create a function that grabs the number of snacks from the input and tells you to stop that many times
 
-document.querySelector('#help').addEventListener('click', runStop)
 
-function runStop(){
+document.querySelector('#help').addEventListener('click', giveHelp)
+
+function giveHelp(){
+    let snack = Number(document.querySelector('#snacks').value)
+
     document.querySelector('#stops').innerText = ''
-    
-    let userInput = Number(document.querySelector('#snacks').value)
 
-    for(let i = 1; i <= userInput; i++){
+    for(let i =1; i <= snack; i++ ){
         document.querySelector('#stops').innerText += `Stop eating snacks\n`
     }
-    
 }
