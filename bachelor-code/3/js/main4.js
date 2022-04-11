340ff6b5db5a8b7e2024182ff78f367d677283d9
coding4freedom <chawkins2010@hotmail.com>
2022-04-11 18:53:28 -0500

    bachelor 3 try 4

--- bachelor-code/3/js/main4.js
+++ bachelor-code/3/js/main4.js
@@ -1,14 +1,11 @@
 let contestants = document.querySelectorAll('.contestant');
 
-Array.from(contestants).forEach(element => {
-   return element.addEventListener('click', checkRose)
-})
+Array.from(contestants).forEach(element => element.addEventListener('click', checkRose));
 
 function checkRose(click){
-
     if(click.target.classList.contains('rose')){
         document.querySelector('#nikki').classList.toggle('hidden')
     }else{
-        alert('wrong ')
+        alert('Wrong!')
     }
 }
